50a1e8e7c1aeb601e3b74f6a4faf9c2f5d4db1e7
陈湛明 <chenzhanming04@foxmail.com>
2024-02-02 11:32:48 +0000

    Refactor main program

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
 import System.Environment (getArgs)
-import Text.SourceCode
+import Text.SourceCode ( textPosition, underlineTextSection )
 import Text.Parser
-    ( parseString, SyntaxError(SyntaxError) )
+    ( parseString, SyntaxError(SyntaxError), Parsed (Parsed) )
 import WACC.Syntax.Parser (program)
 import Data.Foldable (traverse_)
-import Text.AnsiEscape ( gray, red, bold )
+import Text.AnsiEscape ( red, bold )
 import System.Exit (exitWith, ExitCode (ExitFailure))
+import qualified WACC.Syntax.Structure as Syntax
 
 syntaxErrorExit :: IO ()
 syntaxErrorExit = exitWith $ ExitFailure 100
@@ -18,41 +19,43 @@ semanticErrorExit = exitWith $ ExitFailure 200
 main :: IO ()
 main = do
     args <- getArgs
-
     case args of
-
+        
         [] -> putStrLn "Please provide a .wacc file to compile."
         
         _:_:_ -> putStrLn "Too many arguments provided (1 accepted)."
         
         path:_ -> do
+            sourceCode <- readFile path
+            processSourceCode sourceCode
+
+processSourceCode :: String -> IO ()
+processSourceCode sourceCode = case parseString program sourceCode of
+    Left Nothing -> do
+        putStrLn "Unknown syntax error."
+        syntaxErrorExit
+
+    Left (Just (SyntaxError pos error')) -> do
+
+        putStrLn ""
+
+        putStrLn `traverse_`
+            underlineTextSection pos (pos+1) (2, '^', red) sourceCode
+
+        putStrLn ""
+
+        let (row, col) = textPosition sourceCode pos
+        putStrLn $ 
+            red "[Syntax] " ++
+            bold (show (row + 1) ++ ":" ++ show (col + 1)) ++ " " ++
+            show error'
         
-            raw <- readFile path
-            case parseString program raw of
+        putStrLn ""
 
-                Left Nothing -> do
-                    putStrLn "Unknown syntax error."
-                    syntaxErrorExit
+        syntaxErrorExit
 
-                Left (Just (SyntaxError pos error')) -> do
+    Right (Parsed _ ast _) -> semanticCheck ast
 
-                    putStrLn ""
-
-                    putStrLn `traverse_`
-                        underlineTextSection pos (pos+1) (2, '^', red) raw
-
-                    putStrLn ""
-
-                    let (row, col) = textPosition raw pos
-                    putStrLn $ 
-                        red "[Syntax] " ++
-                        bold (show (row + 1) ++ ":" ++ show (col + 1)) ++ " " ++
-                        show error'
-                    
-                    putStrLn ""
-
-                    syntaxErrorExit
-
-                Right {} -> do
-                    putStrLn "Successfully parsed."
-
+semanticCheck :: Syntax.Program -> IO ()
+semanticCheck ast =
+    putStrLn "TODO: Semantic check"
