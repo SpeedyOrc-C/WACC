abcc94e6bb01f370e6b87c80e160d1a3a46b36e4
qlhm <1044622322@qq.com>
2024-02-03 19:35:57 +0000

    Remove useless keyword

--- lib/WACC/Semantics/Error.hs
+++ lib/WACC/Semantics/Error.hs
@@ -12,36 +12,28 @@ data WaccSemanticsErrorType
     | TooFewArguments String Int Int
     | InconsistentTypesInArray Type Type
     | IncompatibleAssignment Type Type
-
     | IncompatibleArgument Type Type
     | RedefinedFunction String
     | RedefinedParameter String
-
     | InvalidRead Type
     | InvalidFree Type
     | InvalidReturn Type
     | ReturnInMain
-
     | InvalidIfCondition Type
     | InvalidWhileCondition Type
-
     | InvalidArray Type
     | InvalidIndex Type
     | InvalidPair Type
-
     | InvalidNot Type
     | InvalidNegate Type
     | InvalidLength Type
     | InvalidOrder Type
     | InvalidCharacter Type
-
     | InvalidArithmetic OperandDirection Type
     | InvalidComparisonLeft Type
     | InvalidComparisonRight ComparisonType Type
     | InvalidEquality Type Type
-    
     | InvalidLogical OperandDirection Type
-    deriving Show
 
 instance Show WaccSemanticsErrorType where
     show :: WaccSemanticsErrorType -> String
