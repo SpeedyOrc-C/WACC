0889011fd86c691900b60f30e57f8d4cb672a97e
Steven Xiang <steven.xiang22@imperial.ac.uk>
2024-02-26 22:59:33 +0000

    Created a bash script to check the outputs of all wacc programs

---
+++ compare_output.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+directory="example/valid/IO/print"
+
+for file in $(find "$directory" -type f); do
+    if [ -f "$file" ]; then
+
+        echo "Checking $file\n"
+
+        filename=$(basename "$file")
+        exec_name="${filename%.*}"
+        file_content=$(cat $file)
+        expected_output=$(echo "$file_content" | sed -n '/Output:/,/Program:/p' | sed '/Output:/d; /Program:/d' | sed -n '/Exit:/q;p' | sed 's/^# //g' | sed 's/^#//g')
+
+        echo "Compiling..."
+        cabal run wacc25 -- "$file" --no-text-deco
+        echo
+
+        if [ $? -eq 0 ]; then
+            echo "Executing..."
+            
+            if gcc -o "$exec_name" -z noexecstack "$exec_name.S"; then
+                echo "\nExecution succeeded!"
+                output=$(./"$exec_name")
+
+                echo "The expected output is: $expected_output"
+                echo "The actual output is: $output"
+                if [ "$expected_output" = "$output" ]; then
+                    echo "The output is correct!"
+                else
+                    echo "The output is wrong!"
+                fi
+                rm $exec_name
+            else
+                echo "\nExecution failed!"
+            fi
+        else
+            echo "compilation of $filename failed"
+        fi
+
+        echo "\n\n"
+
+    fi
+done
+
+rm *.S
