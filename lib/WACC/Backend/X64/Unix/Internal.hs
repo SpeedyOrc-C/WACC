491736d2a0bddd1c2d310a29fce08eeda82d0bee
陈湛明 <chenzhanming04@foxmail.com>
2024-02-27 13:15:47 +0000

    Add print line break

--- lib/WACC/Backend/X64/Unix/Internal.hs
+++ lib/WACC/Backend/X64/Unix/Internal.hs
@@ -35,3 +35,35 @@ printString = Sq.fromList
     Leave,
     Return
     ]
+
+{-
+line_break: .asciz "\n"
+print_line_break:
+    push %rbp
+    mov %rsp, %rbp
+
+    mov $1, %edi
+    lea line_break(%rip), %rsi
+    mov $2, %edx
+    call write
+
+    leave
+    ret
+-}
+printLineBreak :: Sq.Seq Instruction
+printLineBreak = Sq.fromList
+    [
+    Label "line_break", AsciiZero "\n",
+
+    Label "print_line_break",
+    Push (Register (RBP, B8)),
+    Move (Register (RSP, B8)) (Register (RBP, B8)),
+
+    Move (Immediate $ ImmediateInt 1) (Register (RDI, B4)),
+    LoadAddress (MemoryIndirect (Just "line_break") (RIP, B8) Nothing) (Register (RSI, B8)),
+    Move (Immediate $ ImmediateInt 2) (Register (RDX, B4)),
+    Call "write",
+
+    Leave,
+    Return
+    ]
