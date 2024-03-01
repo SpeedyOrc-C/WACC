49aa63e1b7c048beb5dfeaed8c9d40fe67a2bd4e
qlhm <1044622322@qq.com>
2024-03-01 01:34:14 +0000

    Add some more unit tests for expressions

--- test/Test/WACC/Backend/IR.hs
+++ test/Test/WACC/Backend/IR.hs
@@ -6,6 +6,7 @@ import Data.Map as M
 import qualified WACC.Semantics.Structure as SM
 import WACC.IR.Structure
 
+{- Helper function for testing expressions -}
 testExpression :: SM.Expression -> (Scalar, [SingleStatement]) -> IO Bool
 testExpression expr (s, ss) =
     case runIdentity (evalStateT (expression expr) (initialState M.empty)) of
@@ -14,10 +15,24 @@ testExpression expr (s, ss) =
             then return True
             else return False
 
-testBool :: IO Bool
-testBool = testExpression (SM.LiteralBool True) (Immediate 1, [])
+{- Tests for expressions. -}
+testLiteralBool :: IO Bool
+testLiteralBool = testExpression (SM.LiteralBool True) (Immediate 1, [])
+
+testLiteralInt :: IO Bool
+testLiteralInt  = testExpression (SM.LiteralInt 5) (Immediate 5, [])
+
+testLiteralChar :: IO Bool
+testLiteralChar  = testExpression (SM.LiteralChar 'c') (Immediate 99, [])
+
+testLiteralPairNull :: IO Bool
+testLiteralPairNull  = testExpression SM.LiteralPairNull (Immediate 0, [])
+
 
 irUnitTests :: IO [Bool]
 irUnitTests = sequence [
-    testBool
+    testLiteralBool,
+    testLiteralInt,
+    testLiteralChar,
+    testLiteralPairNull
   ]
