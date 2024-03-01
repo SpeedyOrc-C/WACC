cf17dd16335737171d08e5d381e804996e358552
陈湛明 <chenzhanming04@foxmail.com>
2024-03-01 10:33:49 +0000

    Fix script "compile"'s exit code

--- lib/WACC/IR/FlattenExpression.hs
+++ lib/WACC/IR/FlattenExpression.hs
@@ -143,7 +143,7 @@ expression = \case
         tmp <- newTemporary
         return (Variable tmp,
             evaluate ++
-            [Assign (getSize t) tmp (Call (getSize t) ("fn." ++ f)
+            [Assign (getSize t) tmp (Call (getSize t) ("fn_" ++ f)
             ((getSize <$> ts) `zip` scalars))])
     where
     unary :: Size -> (Scalar -> Expression) -> SM.Expression
@@ -457,7 +457,7 @@ program (SM.Program fs main) = do
             = SM.Function SM.Int "main" [] main
                 : map
                     (\(SM.Function ret name param b) ->
-                        SM.Function ret ("fn." ++ name) param b)
+                        SM.Function ret ("fn_" ++ name) param b)
                     (S.toList fs)
     functions' <- functions allFunctions
     return $ Program dataSegment functions'
