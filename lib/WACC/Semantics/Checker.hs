d469ca170a58b8a175f014a85a2df665e470ff3f
Joe <jz6122@ic.ac.uk>
2024-03-15 16:46:39 +0000

    [jz6122] enabling nested struct being evaluated by newstruct

--- lib/WACC/Semantics/Checker.hs
+++ lib/WACC/Semantics/Checker.hs
@@ -287,9 +287,12 @@ triedToAssigns  (expressionRange -> range)
                 (Struct k _) 
                 (Struct a _)   = Ok (k == a)
 
-triedToAssigns (expressionRange -> range) declaredType computedType 
-    = Log [SemanticError range $
-                IncompatibleAssignment declaredType computedType]
+triedToAssigns right leftType rightType
+    = if triedToAssign right leftType rightType then do
+        Ok True
+        else Log [SemanticError (expressionRange right) $
+                        IncompatibleAssignment leftType rightType]
+
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (x,y,z)= f x y z
