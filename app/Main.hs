3a45f6bf8d94efd5bacf4673a838ff3294b4a09c
陈湛明 <chenzhanming04@foxmail.com>
2024-04-14 21:05:12 +0800

    Refactor "main"
    Improve flag handling and error message.

--- app/Main.hs
+++ app/Main.hs
@@ -1,64 +1,29 @@
 module Main where
 
-import Prelude hiding (error)
-import qualified Prelude as P
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.Info
 
-import System.Environment ( getArgs )
-import System.FilePath ( takeBaseName )
-import System.Exit ( ExitCode(ExitFailure), exitSuccess, exitWith )
+import Control.Arrow
+import Control.Monad
+import Data.Foldable
+import Data.List
 
-import Control.Monad ( when )
-import Data.Foldable ( for_, traverse_ )
-import Data.Function ((&))
-import qualified Data.Sequence as Sq
+import Text.SourceCode
+import PrettyPrint
 
-import Text.SourceCode ( textPosition, underlineTextSection, removeTabs )
-import Text.AnsiEscape ( bold, red )
-import qualified Text.Parser              as Parser
-import qualified WACC.Syntax.Parser       as Syntax.Parser
-import qualified WACC.Syntax.Structure    as Syntax.Structure
-import qualified WACC.Semantics.Checker   as Semantics.Checker
-import qualified WACC.Semantics.Structure as Semantics.Structure
-import qualified WACC.Semantics.Utils     as Semantics.Utils
-import qualified WACC.Semantics.Error     as Semantics.Error
-import qualified WACC.IR.Generate as IR
-import qualified WACC.Backend.X64.Generate as X64
-import qualified WACC.Backend.X64.ATnT as ATnT
-
-import qualified WACC.Backend.X64.Unix.Config as Unix
-import qualified WACC.Backend.X64.Windows.Config as Windows
-
-syntaxErrorExit :: IO ()
-syntaxErrorExit = exitWith $ ExitFailure 100
-
-semanticErrorExit :: IO ()
-semanticErrorExit = exitWith $ ExitFailure 200
-
-splitFlags :: [String] -> ([String], [String])
-splitFlags args = (filter ((== "--") . take 2) args, filter ((/= "--") . take 2) args)
-
-data Target = Unix | Windows deriving Show
-
-data Flags = Flags {
-    noTextDecoration :: Bool,
-    showIR :: Bool,
-    target :: Target
-} deriving Show
-
-flagsFromArgs :: [String] -> Flags
-flagsFromArgs args = Flags {
-    noTextDecoration = "--no-text-deco" `elem` args,
-    showIR = "--show-ir" `elem` args,
-    target = case ("--target-unix" `elem` args, "--target-windows" `elem` args) of
-        (True, False) -> Unix
-        (False, True) -> Windows
-        (False, False) -> P.error "Target is not specified."
-        (True, True) -> P.error "So confusing. Please only choose one target."
-}
-
-preventTextDecoration :: Bool -> (a -> a) -> a -> a
-preventTextDecoration False color = color
-preventTextDecoration True _ = id
+import Text.Parser                     qualified as Parser
+import WACC.Syntax.Parser              qualified as Syntax.Parser
+import WACC.Syntax.Structure           qualified as Syntax.Structure
+import WACC.Semantics.Checker          qualified as Semantics.Checker
+import WACC.Semantics.Structure        qualified as Semantics.Structure
+import WACC.Semantics.Utils            qualified as Semantics.Utils
+import WACC.IR.Generate                qualified as IR
+import WACC.Backend.X64.Generate       qualified as X64
+import WACC.Backend.X64.ATnT           qualified as ATnT
+import WACC.Backend.X64.Unix.Config    qualified as Unix
+import WACC.Backend.X64.Windows.Config qualified as Windows
 
 main :: IO ()
 main = do
@@ -69,109 +34,101 @@ main = do
 
         _:_:_ -> putStrLn "Too many arguments provided (1 accepted)."
 
-        path:_ -> do
-            let targetUnix = "--target-unix" `elem` flagsArgs
-            let targetWindows = "--target-windows" `elem` flagsArgs
+        [path] -> do
+            sourceCode <- readFile path
 
             let flags = flagsFromArgs flagsArgs
+            let filBaseName = takeBaseName path
+            let frontendResult =
+                    checkSyntax flags sourceCode >>=
+                    checkSemantics flags sourceCode
 
-            case (targetUnix, targetWindows) of
-                (True, False) -> do
-                    sourceCode <- readFile path
-                    processSourceCode path flags {target=Unix} sourceCode
-                (False, True) -> do
-                    sourceCode <- readFile path
-                    processSourceCode path flags {target=Windows} sourceCode
-                (True, True) ->
-                    putStrLn "So confusing. Please specify only one target."
-                (False, False) ->
-                    putStrLn "Please specify a target."
+            case frontendResult of
+                Left (exit, errors) -> do
+                    traverse_ putStrLn errors
+                    exit
 
-processSourceCode :: FilePath -> Flags -> String -> IO ()
-processSourceCode path flags (removeTabs -> sourceCode) =
+                Right program -> generateCode filBaseName flags program
+
+--------------------------------------------------------------------------------
+
+type FileBaseName = String
+
+data Target = Unix | Windows deriving Show
+
+data Flags = Flags {
+    noTextDecoration :: Bool,
+    showIR :: Bool,
+    targetWindows :: Bool,
+    targetUnix :: Bool
+} deriving Show
+
+syntaxErrorExit :: IO ()
+syntaxErrorExit = exitWith $ ExitFailure 100
+
+semanticErrorExit :: IO ()
+semanticErrorExit = exitWith $ ExitFailure 200
+
+splitFlags :: [String] -> ([String], [String])
+splitFlags = filter ((== "--") . take 2) &&& filter ((/= "--") . take 2)
+
+flagsFromArgs :: [String] -> Flags
+flagsFromArgs args = Flags {
+    noTextDecoration = "--no-text-deco" `elem` args,
+    showIR = "--show-ir" `elem` args,
+    targetUnix = "--target-unix" `elem` args,
+    targetWindows = "--target-windows" `elem` args
+}
+
+preventTextDecoration :: Bool -> (a -> a) -> a -> a
+preventTextDecoration False color = color
+preventTextDecoration True _ = id
+
+checkSyntax :: Flags -> SourceCode -> Either (IO (), [String]) Syntax.Structure.Program
+checkSyntax flags (removeTabs -> sourceCode) =
     case Parser.parseString Syntax.Parser.program sourceCode of
+        Right (Parser.Parsed _ ast _) -> Right ast
+        Left Nothing -> Left
+            (syntaxErrorExit, return "Unknown syntax error.")
+        Left (Just e) -> Left
+            (syntaxErrorExit, prettyPrintError (noTextDecoration flags) sourceCode e)
 
-    Left Nothing -> do
-        putStrLn "Unknown syntax error."
-        syntaxErrorExit
-
-    Left (Just (Parser.SyntaxError pos error')) -> do
-        let (row, col) = textPosition sourceCode pos
-
-        putStrLn ""
-
-        putStrLn $
-            preventTextDecoration (noTextDecoration flags) red
-                "[Syntax Error] " ++
-            preventTextDecoration (noTextDecoration flags) bold
-                (show (row + 1) ++ ":" ++ show (col + 1))
-
-        print error'
-
-        (putStrLn . ("|   " ++ )) `traverse_`
-            underlineTextSection pos (pos+1)
-                (2, '^', preventTextDecoration (noTextDecoration flags) red)
-                sourceCode
-
-        putStrLn ""
-
-        syntaxErrorExit
-
-    Right (Parser.Parsed _ ast _) -> semanticCheck path flags ast sourceCode
-
-semanticCheck :: FilePath -> Flags -> Syntax.Structure.Program -> String -> IO ()
-semanticCheck path flags program sourceCode =
+checkSemantics :: Flags -> SourceCode -> Syntax.Structure.Program
+    -> Either (IO (), [String]) Semantics.Structure.Program
+checkSemantics flags sourceCode program =
     case Semantics.Checker.checkProgram program of
+        Semantics.Utils.Ok ast -> Right ast
+        Semantics.Utils.Log semanticErrors -> Left $ (semanticErrorExit, ) $
+            intercalate [""] $
+                prettyPrintError (noTextDecoration flags) sourceCode <$>
+                    semanticErrors
 
-    Semantics.Utils.Ok ast -> do
-        generateCode path flags ast
-
-    Semantics.Utils.Log semanticErrors -> do
-
-        putStrLn ""
-
-        for_ semanticErrors $ \(Semantics.Utils.SemanticError range error) -> do
-            let (from, to) = range
-            let (fromRow, fromCol) = textPosition sourceCode from
-
-            putStrLn $
-                preventTextDecoration (noTextDecoration flags)
-                    red "[Semantic Error] " ++
-                preventTextDecoration (noTextDecoration flags) bold
-                    (show (fromRow + 1) ++ ":" ++ show (fromCol + 1)) ++ " "
-
-            error & \case
-                Semantics.Error.RedefinedIdentifier
-                    name (textPosition sourceCode -> (pos, _))
-                    -> putStrLn $
-                        "Variable \"" ++ name ++ "\" is declared again.\n" ++
-                        "Previously defined at line " ++ show (pos + 1) ++ "."
-                it -> print it
-
-            (putStrLn . ("|   " ++)) `traverse_`
-                underlineTextSection from to
-                    (2, '^', preventTextDecoration (noTextDecoration flags) red)
-                    sourceCode
-
-            putStrLn ""
-
-        semanticErrorExit
-
-generateCode :: FilePath -> Flags -> Semantics.Structure.Program -> IO ()
-generateCode path flags ast = do
-    let name = takeBaseName path
-
-    let ir = IR.generateIR ast
+generateCode :: FileBaseName -> Flags -> Semantics.Structure.Program -> IO ()
+generateCode fileBaseName flags program = do
+    let ir = IR.generateIR program
     when (showIR flags) $ IR.debug ir
 
-    let asm = (`X64.generateX64` ir) $ case target flags of
-            Unix -> Unix.config
-            Windows -> Windows.config
+    case eitherTarget of
+        Left msg -> putStrLn msg
+        Right target -> do
+            let asm = X64.generateX64 target ir
+            let atntLines = ATnT.atnt <$> asm
 
-    let output = asm >>= \i -> ATnT.atnt i Sq.|> "\n"
-    let fileName = name ++ ".S"
-
-    writeFile fileName ""
-    for_ output $ appendFile fileName
-
-    exitSuccess
+            let asmFileName = fileBaseName ++ ".S"
+            writeFile asmFileName ""
+            for_ atntLines $ \line -> do
+                traverse_ (appendFile asmFileName) line
+                appendFile asmFileName "\n"
+    where
+    eitherTarget = case (targetUnix flags, targetWindows flags) of
+        (True, False) -> Right Unix.config
+        (False, True) -> Right Windows.config
+        (True, True) -> Left
+            "So confusing. Please specify only one target."
+        (False, False) -> case os of
+            "mingw32" -> Right Windows.config
+            "linux" -> Right Unix.config
+            "darwin" -> Right Unix.config
+            _ -> Left $
+                "Please specify a target, " ++
+                "this OS (" ++ os ++ ") is not supported."
---
+++ app/PrettyPrint.hs
@@ -0,0 +1,59 @@
+module PrettyPrint where
+
+import Text.SourceCode ( textPosition, underlineTextSection )
+import Text.AnsiEscape ( bold, red )
+import Text.Parser (SyntaxError(SyntaxError))
+import WACC.Syntax.Error (WaccSyntaxErrorType)
+import WACC.Semantics.Utils (SemanticError (SemanticError))
+import WACC.Semantics.Error (WaccSemanticsErrorType(RedefinedIdentifier))
+
+type SourceCode = String
+type NoStyle = Bool
+
+class PrettyError error where
+    errorTitle :: error -> String
+
+    errorRange :: error -> (Int, Maybe Int)
+
+    errorDescription :: SourceCode -> error -> String
+
+    prettyPrintError :: PrettyError error =>
+        NoStyle -> SourceCode -> error -> [String]
+    prettyPrintError noStyle code e =
+        [stylishTitle ++ " " ++ stylishPosition, description] ++ codeSection
+        where
+            (~) c = if noStyle then id else c
+
+            (row, col) = textPosition code (fst $ errorRange e)
+            (from, to') = errorRange e
+            to = case to' of Just t -> t; Nothing -> from + 1
+
+            stylishTitle = red ~ ("[" ++ errorTitle e ++ "] ")
+            stylishPosition = bold ~ (show row ++ ":" ++ show col)
+            description = errorDescription code e
+            codeSection = ("|   " ++) <$>
+                underlineTextSection from to (2, '^', (red ~)) code
+
+instance PrettyError (SyntaxError WaccSyntaxErrorType) where
+    errorTitle :: SyntaxError WaccSyntaxErrorType -> String
+    errorTitle = const "Syntax Error"
+
+    errorRange :: SyntaxError WaccSyntaxErrorType -> (Int, Maybe Int)
+    errorRange (SyntaxError pos _) = (pos, Nothing)
+
+    errorDescription :: SourceCode -> SyntaxError WaccSyntaxErrorType -> String
+    errorDescription _ = show
+
+instance PrettyError SemanticError where
+    errorTitle :: SemanticError -> String
+    errorTitle = const "Semantic Error"
+
+    errorRange :: SemanticError -> (Int, Maybe Int)
+    errorRange (SemanticError (from, to) _) = (from, Just to)
+
+    errorDescription :: SourceCode -> SemanticError -> String
+    errorDescription code (SemanticError _ e) = case e of
+        RedefinedIdentifier name (textPosition code -> (pos, _)) ->
+            "Variable \"" ++ name ++ "\" is declared again.\n" ++
+            "Previously defined at line " ++ show (pos + 1) ++ "."
+        it -> show it
